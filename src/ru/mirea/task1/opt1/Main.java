eeb032411bd065efef1beea6c2cb05d3ae4b454c
Tronor <romankuzmenko73q@gmail.com>
2021-09-15 10:12:26 +0300

    added lab1-sum

---
+++ src/ru/mirea/task1/opt1/Main.java
@@ -0,0 +1,20 @@
+package ru.mirea.task1.opt1;
+
+import java.lang.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+        int[] arr = new int[n];
+
+        for(int i = 0; i<n;i++) {
+            int num = sc.nextInt();
+            arr[i] = num;
+            sum += num;
+        }
+        System.out.println(sum);
+    }
+}
