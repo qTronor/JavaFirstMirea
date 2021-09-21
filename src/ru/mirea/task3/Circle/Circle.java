0d9816450473020113205f5bf05f2998cd28becb
qTronor <romankuzmenko73q@gmail.com>
2021-09-21 20:04:59 +0300

    Added classes Book and BookTest for task3

---
+++ src/ru/mirea/task3/Circle/Circle.java
@@ -0,0 +1,18 @@
+package ru.mirea.task3.Circle;
+
+import java.lang.*;
+
+public class Circle {
+    private int radius;
+    public Circle(int r){
+        radius = r;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+}
---
+++ src/ru/mirea/task3/Circle/CircleTest.java
@@ -0,0 +1,11 @@
+package ru.mirea.task3.Circle;
+
+import ru.mirea.task3.Circle.Circle;
+
+import java.lang.*;
+public class CircleTest {
+    public static void main(String[] args) {
+        Circle c1 = new Circle(25);
+        System.out.println("Radius = " + c1.getRadius());
+    }
+}
