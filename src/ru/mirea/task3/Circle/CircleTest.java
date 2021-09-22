e05990677ec6db29796df087721b630dbf92e4e9
qTronor <romankuzmenko73q@gmail.com>
2021-09-22 09:21:29 +0300

    Added classes Circle and CircleTest for task3

--- src/ru/mirea/task3/Circle/CircleTest.java
+++ src/ru/mirea/task3/Circle/CircleTest.java
@@ -7,5 +7,6 @@ public class CircleTest {
     public static void main(String[] args) {
         Circle c1 = new Circle(25);
         System.out.println("Radius = " + c1.getRadius());
+        System.out.println(c1);
     }
 }
