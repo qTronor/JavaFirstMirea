96f842a11d5cefd3b93e11a6d84429067af59ad5
qTronor <romankuzmenko73q@gmail.com>
2021-09-22 10:31:19 +0300

    Added classe ShapeTest for task2

--- src/ru/mirea/task2/shape/Shape.java
+++ src/ru/mirea/task2/shape/Shape.java
@@ -5,4 +5,17 @@ import java.lang.*;
 public class Shape {
     private String color;
     private boolean filled;
+
+    public Shape(String color, boolean filled){
+        this.color = color;
+        this.filled = filled;
+    }
+
+    @Override
+    public String toString() {
+        return "Shape{" +
+                "color='" + color + '\'' +
+                ", filled=" + filled +
+                '}';
+    }
 }
---
+++ src/ru/mirea/task2/shape/ShapeTest.java
@@ -0,0 +1,8 @@
+package ru.mirea.task2.shape;
+
+public class ShapeTest {
+    public static void main(String[] args) {
+        Shape s1 = new Shape("Blue", true);
+        System.out.println(s1);
+    }
+}
